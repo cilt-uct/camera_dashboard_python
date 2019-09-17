06c1b3b25c959ab3141f6d6aaf92b987bda24903
Ngonidzashe Choga <ngonidzashe.choga@uct.ac.za>
2019-09-17 14:47:50 +0200

    OPENCAST-2647 - work-around for how nginx is handling urls

--- sync/urls.py
+++ sync/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard, name='dashboard'),
-    path("online/", views.online, name='online'),
-    path("offline/", views.offline, name='offline'),
-    path("venues/", views.venues, name='venues'),
-    path("ca.json", views.cas),
+    path("ip-cam/", views.dashboard, name='dashboard'),
+    path("ip-cam/online/", views.online, name='online'),
+    path("ip-cam/offline/", views.offline, name='offline'),
+    path("ip-cam/venues/", views.venues, name='venues'),
+    path("ip-cam/ca.json", views.cas),
 ]
